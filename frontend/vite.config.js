3d5a78b328a583a9df94d3ced6510fb6ac7033ae
Exxotelis <telis0kaps@gmail.com>
2025-08-24 14:51:15 +0100

    spa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/static/spa/',   // ✅ ΣΩΣΤΟ για SPA served από Django
+  base: '/static/spa/',   
 })
